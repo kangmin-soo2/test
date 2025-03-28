dd800042a43478c4c0c711f8f442d6df5aac66f4
kangmin-soo2 <kangms1234@kyonggi.ac.kr>
2025-03-28 19:31:53 +0900

    test commit

--- week2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/min/week2/src
-# Build directory: /home/min/week2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("week2_pkg")
--- week2/build/week2_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/min/week2/src/week2_pkg
-# Build directory: /home/min/week2/build/week2_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
